ea930a62e51fc7ed960907fac56932a94559554c
Saeed812 <134283764+Saeed812@users.noreply.github.com>
2024-04-21 20:22:30 -0700

    react13

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Header from './componet/Header'
+import Footer from './componet/Footer'
+
+
+function App() {
+  return (
+   
+   <div>
+        <Header/>
+        <main>
+          <h1> this website writed by react js</h1> 
+        </main>
+        <Footer/>
+   </div>
+  
+  );
+}
+
+export default App;
---
+++ frontend/src/componet/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+       
+       <Footer>this is footer</Footer>
+
+    </div>
+  )
+}
+
+export default Footer
