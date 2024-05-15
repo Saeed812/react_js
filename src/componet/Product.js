8a42ee8905c055c439e811e30da6c6c4a5d5392c
Mohammad Kaab <mohammad.kaab.ext@k-tronik.digital>
2024-05-16 01:50:04 +0200

    Make the product list available

--- src/componet/Product.js
+++ src/componet/Product.js
@@ -4,12 +4,9 @@ import { Card } from 'react-bootstrap'
 function Product({ product }) {
   return (
     <div>
-          <Card className="p-3 my-3 rounded">
-          <a href={'/product/=${product._id}'}></a>
-          
-          </Card>
-
-
+      <Card className="p-3 my-3 rounded">
+        <a href={`/product/${product._id}`}>{product.name}</a>
+      </Card>
     </div>
   )
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,17 +7,12 @@ import Product from '../componet/Product'
 function HomeScreen() {
   return (
     <div>
-        
         <h1>Latest Product</h1>
         <Row>
          {products.map(product=> (
-          
-
-          <Col key={product._id} sm={12} md={6} lg ={4} xl={3}>
-            <Product product ={product}/>
-
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product}/>
           </Col>
-
          ))}
        </Row>
 
